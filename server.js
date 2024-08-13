1fa8d16ffee3aa101b9d6b0c8c117963780e03fd
KarenBareiro <karennbenitez@hotmail.com>
2024-08-13 02:30:04 -0400

    Actualización del servidor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const userRoutes = require('./routes/userRoutes')
 
 
 require('./config/database.config') 
@@ -8,6 +9,8 @@ require('./config/database.config')
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/user', userRoutes);
 
 
 app.listen(8080, () => {
